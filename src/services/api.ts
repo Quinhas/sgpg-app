74400266c4dcc197784d356ed279bdfff4f7d27c
Lucas Santana <ln.santana08@gmail.com>
2021-11-24 00:19:25 -0300

    fix: api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -17,12 +17,10 @@ import { Role, RoleDTO } from "src/types/role.interface";
 import { Student, StudentDTO } from "src/types/student.interface";
 
 const apiService = axios.create({
-  // baseURL:
-  //   process.env.NODE_ENV === "production"
-  //     ? "https://sgpg-univem.herokuapp.com/"
-  //     : "http://localhost:5000/",
-  // baseURL: "https://sgpg-univem.herokuapp.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://sgpg-univem.herokuapp.com/"
+      : "http://localhost:5000/",
 });
 
 // BEGINS EMPLOYEE SERVICE
